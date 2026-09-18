f0fa52585949c3dc163699c86d2fe65b0403ed62
diytramboss <diytramboss@outlook.com>
2026-09-18 22:27:48 +0700

    stat work on subsytms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/gimbal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/gimbal.java
@@ -1,3 +1,28 @@
-public class gimbal {
-    
+package org.firstinspires.ftc.teamcode.subsys;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+
+public class Gimbal {
+    CRServo YServo = hardwareMap.get(CRServo.class, "Servo1");
+    CRServo ZServo = hardwareMap.get(CRServo.class, "Servo2");
+
+    static public final float map(double value,
+                                  double istart,
+                                  double istop,
+                                  double ostart,
+                                  double ostop) {
+        return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+    }
+
+    public void trackTag(double bearing, double elevation){
+        boolean negativeBearing = (bearing < 0);
+        boolean negativeElevation = (elevation < 0);
+
+        YServo.setPower(map(bearing, 0, 360.0, 0, 1.0));
+        ZServo.setPower(map(elevation, 0, 360.0, 0, 1.0));
+
+        // add failsafes if... fail duh. maybe
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/shooting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsys/shooting.java
@@ -1,3 +1,12 @@
+package org.firstinspires.ftc.teamcode.subsys;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class shooting {
-    
+    DcMotorEx feeder = hardwareMap.get(DcMotorEx.class, "feeder");
+    DcMotorEx shooter = hardwareMap.get(DcMotorEx.class, "shoot");
 }
